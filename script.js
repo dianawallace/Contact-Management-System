ce172a155a1271083e7bf4b30f310696a1dfff61
Diana Wallace <dianawallacedeveloper@gmail.com>
2019-09-22 10:37:36 -0600

    created cmsTable and cmsTableDemo variable

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ clearBtn.addEventListener('click', () => {
     localStorage.removeItem(tableKey)
 });
 
-
+let cmsTable;
+let cmsTableDemo = {
+    'Dave Rapoza': {
+        'phone': '766-786-7868',
+        'address': '1234 Sunshine Ave, Denver, Co, 80125'
+    },
+    'Wylie Beckert': {
+        'phone': '555-123-6789',
+        'address': '9876 Artist Way, Seattle, WA, 66541'
+    }    
+}; 
